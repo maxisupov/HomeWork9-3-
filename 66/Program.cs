f8c8c7bec09f13b4a660c4ea984c5d04d0034d9f
Maxim <max_isupov@mail.ru>
2022-10-20 00:58:29 +0300

    Решил задачу 66

---
+++ 66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите чило M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите чило N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void SumNumbers (int m, int n, int sum)
+{
+    if(m > n)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N = {sum}");
+        return;
+    } 
+    sum = sum + (m++);
+    SumNumbers(m, n, sum);
+}
+
+SumNumbers(m, n, 0);
+
